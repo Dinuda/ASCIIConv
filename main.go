3427478fab0f2ae882525d8f5f2d7948e5011517
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-19 11:32:31 +0530

    char to ascii

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Print("1. ASCII to char \n")
-	fmt.Print("2. char to ASCII \n")
+	fmt.Println("1. ASCII to char")
+	fmt.Println("2. char to ASCII")
 	var req string
 
 	fmt.Println("What is your requirment: ")
 	fmt.Scanln(&req)
+	if req == "1" {
+		// fmt.Println("Enter character value: ")
+		// var in string
+		// fmt.Scanln(&in)
+		// println(in)
+		// c := 'A' // rune (characters in Go are represented using `rune` data type)
+		// asciiValue := int(c)
+		// fmt.Printf("Ascii Value of %c = %d\n", c, asciiValue)
+
+	}
+	if req == "2" {
+		fmt.Println("Enter character value: ")
+		var in int
+		fmt.Scanln(&in)
+		asciiValue := in
+		character := rune(asciiValue)
+		fmt.Printf("Character corresponding to Ascii Code %d = %c\n", asciiValue, character)
+	}
 
 }
