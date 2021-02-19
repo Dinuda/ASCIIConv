1d9d290cec5c95266243ddaa5e83f8e496cc22ea
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-19 11:14:39 +0530

    main fie

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("1. ASCII to char \n")
+	fmt.Print("2. char to ASCII \n")
+	var req string
+
+	fmt.Println("What is your requirment: ")
+	fmt.Scanln(&req)
+
+}
