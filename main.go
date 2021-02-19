2fda7f9a6937113f45d666e10792f313f81cc0ff
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-19 11:34:34 +0530

    ascii to char

--- main.go
+++ main.go
@@ -12,14 +12,9 @@ func main() {
 	fmt.Println("What is your requirment: ")
 	fmt.Scanln(&req)
 	if req == "1" {
-		// fmt.Println("Enter character value: ")
-		// var in string
-		// fmt.Scanln(&in)
-		// println(in)
-		// c := 'A' // rune (characters in Go are represented using `rune` data type)
-		// asciiValue := int(c)
-		// fmt.Printf("Ascii Value of %c = %d\n", c, asciiValue)
-
+		c := 'A' // rune (characters in Go are represented using `rune` data type)
+		asciiValue := int(c)
+		fmt.Printf("Ascii Value of %c = %d\n", c, asciiValue)
 	}
 	if req == "2" {
 		fmt.Println("Enter character value: ")
